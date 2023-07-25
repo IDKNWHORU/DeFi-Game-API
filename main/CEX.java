aff4663ef3705d7e90db9ac3a18d2bc52c231659
IDKNWHORU <junho9341@gmail.com>
2023-07-25 13:11:17 +0900

    add purchase SToken in CEX

--- main/CEX.java
+++ main/CEX.java
@@ -17,27 +17,57 @@ public class CEX {
         this.assetMap.put("e", new Asset());
     }
 
+    public void purchase(String assetKey, String tokenType, int quantity) {
+        Asset target = this.getAssetMap().get(assetKey);
+
+        if(tokenType.equals("SToken")) {
+            System.out.println("tokenType : " + tokenType);
+            target.purchaseSToken(quantity);
+        }
+    }
+
     public Map<String, Asset> getAssetMap() { return this.assetMap; }
 
-    private class Asset {
+    public class Asset {
         private int money;
-        private int sToken;
-        private int lToken;
+        private int SToken;
+        private int LToken;
 
         Asset() {
-            this.money = 2000;
-            this.sToken = 0;
-            this.lToken = 0;
+            this.money = 200000;
+            this.SToken = 0;
+            this.LToken = 0;
+        }
+
+        public int getMoney() {
+            return this.money;
+        }
+
+        public int getSToken() {
+            return this.SToken;
+        }
+
+        public int getLToken() {
+            return this.LToken;
+        }
+
+        public boolean purchaseSToken(int quantity) {
+            int price = quantity * 1000;
+            if(price <= this.money) {
+                this.SToken = quantity;
+                this.money = this.money - price;
+            }
+            return true;
         }
 
         public String toString() {
             return """
                     Asset {
                         money: %s,
-                        sToken: %s,
-                        lToken: %s
+                        SToken: %s,
+                        LToken: %s
                     }
-                    """.formatted(this.money, this.sToken, this.lToken);
+                    """.formatted(this.money, this.SToken, this.LToken);
         }
     }
 }
--- test/CEXTest.java
+++ test/CEXTest.java
@@ -3,16 +3,38 @@ package test;
 import main.CEX;
 
 public class CEXTest {
-    // cex init will return 5 Assets = [a, b, c, d, e]
+    static CEX cex = new CEX();
+
     public static void main(String... args) {
-        CEX cex = new CEX();
+        checkInitialize();
+        aPurchase200SToken();
+    }
 
+    // cex init will return 5 Assets = [a, b, c, d, e]
+    public static void checkInitialize() {
+        System.out.println("***** 게임 시작할 때 CEX 초기화 *****");
         cex.init();
+        System.out.println("cex size will be 5: "+ (cex.getAssetMap().size() == 5));
 
-        System.out.println(cex.getAssetMap().size() == 5);
+        CEX.Asset a = cex.getAssetMap().get("a");
 
-        Object a = cex.getAssetMap().get("a");
+        System.out.println("show Asset instance");
+        System.out.println(a);
+        System.out.println("a have 2000 money: " + (a.getMoney() == 200000));
+        System.out.println("a have 0 SToken: " + (a.getSToken() == 0));
+        System.out.println("a have 0 LToken: " + (a.getLToken() == 0));
+        System.out.println("***** CEX 초기화 끝 값 확인 완료 *****");
+    }
+
+    public static void aPurchase200SToken() {
+        System.out.println("***** CEX에서 A지갑이 200 SToken을 구매 테스트 시작 *****");
+        CEX.Asset a = cex.getAssetMap().get("a");
+
+        cex.purchase("a", "SToken", 200);
 
         System.out.println(a);
+        System.out.println("a have 0 money: " + (a.getMoney() == 0));
+        System.out.println("a have 200 SToken: " + (a.getSToken() == 200));
+        System.out.println("***** CEX에서 A지갑이 200 SToken을 구매 테스트 종료 *****");
     }
 }
