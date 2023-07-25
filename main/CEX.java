ada7506ab7b2e720d002f43adc966aafd5e640c6
IDKNWHORU <junho9341@gmail.com>
2023-07-25 13:25:36 +0900

    add sale SToken in CEX

--- main/CEX.java
+++ main/CEX.java
@@ -2,6 +2,7 @@ package main;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 public class CEX {
     Map<String, Asset> assetMap;
@@ -17,12 +18,19 @@ public class CEX {
         this.assetMap.put("e", new Asset());
     }
 
-    public void purchase(String assetKey, String tokenType, int quantity) {
+    public void purchase(String assetKey, String tokenType, int quantity) throws Exception {
         Asset target = this.getAssetMap().get(assetKey);
 
         if(tokenType.equals("SToken")) {
-            System.out.println("tokenType : " + tokenType);
             target.purchaseSToken(quantity);
+        }
+    }
+
+    public void sale(String assetKey, String tokenType, int quantity) throws Exception {
+        Asset target = this.getAssetMap().get(assetKey);
+
+        if(tokenType.equals("SToken")) {
+            target.saleSToken(quantity);
         }
     }
 
@@ -51,11 +59,24 @@ public class CEX {
             return this.LToken;
         }
 
-        public boolean purchaseSToken(int quantity) {
+        public boolean purchaseSToken(int quantity) throws Exception {
             int price = quantity * 1000;
             if(price <= this.money) {
-                this.SToken = quantity;
+                this.SToken = this.SToken + quantity;
                 this.money = this.money - price;
+            } else {
+                throw new Exception("money is lower then price");
+            }
+            return true;
+        }
+
+        public boolean saleSToken(int quantity) throws Exception{
+            int price = quantity * 1000;
+            if(quantity <= this.SToken) {
+                this.SToken = this.SToken - quantity;
+                this.money = this.money + price;
+            } else {
+                throw new Exception("you have lower SToken then input quantity");
             }
             return true;
         }
--- test/CEXTest.java
+++ test/CEXTest.java
@@ -5,36 +5,48 @@ import main.CEX;
 public class CEXTest {
     static CEX cex = new CEX();
 
-    public static void main(String... args) {
+    public static void main(String... args) throws Exception {
         checkInitialize();
         aPurchase200SToken();
+        aSale20SToken();
     }
 
     // cex init will return 5 Assets = [a, b, c, d, e]
     public static void checkInitialize() {
         System.out.println("***** 게임 시작할 때 CEX 초기화 *****");
         cex.init();
-        System.out.println("cex size will be 5: "+ (cex.getAssetMap().size() == 5));
+        System.out.println("cex에 정의된 자산(사용자 자산) 카운트 5개: "+ (cex.getAssetMap().size() == 5));
 
         CEX.Asset a = cex.getAssetMap().get("a");
 
-        System.out.println("show Asset instance");
+        System.out.println("사용자 A의 자산 정보");
         System.out.println(a);
-        System.out.println("a have 2000 money: " + (a.getMoney() == 200000));
-        System.out.println("a have 0 SToken: " + (a.getSToken() == 0));
-        System.out.println("a have 0 LToken: " + (a.getLToken() == 0));
+        System.out.println("a의 현금 보유액 200000원: " + (a.getMoney() == 200000));
+        System.out.println("a의 보유 SToken 0개: " + (a.getSToken() == 0));
+        System.out.println("a의 보유 LToken 0개: " + (a.getLToken() == 0));
         System.out.println("***** CEX 초기화 끝 값 확인 완료 *****");
     }
 
-    public static void aPurchase200SToken() {
+    public static void aPurchase200SToken() throws Exception {
         System.out.println("***** CEX에서 A지갑이 200 SToken을 구매 테스트 시작 *****");
         CEX.Asset a = cex.getAssetMap().get("a");
 
         cex.purchase("a", "SToken", 200);
 
-        System.out.println(a);
-        System.out.println("a have 0 money: " + (a.getMoney() == 0));
-        System.out.println("a have 200 SToken: " + (a.getSToken() == 200));
-        System.out.println("***** CEX에서 A지갑이 200 SToken을 구매 테스트 종료 *****");
+        System.out.println("a의 현금 보유액은 0원: " + (a.getMoney() == 0));
+        System.out.println("a의 보유 SToken은 200개: " + (a.getSToken() == 200));
+        System.out.println("***** CEX에서 A지갑이 200 SToken을 구매 테스트 완료 *****");
+    }
+
+    public static void aSale20SToken() throws Exception {
+        System.out.println("***** CEX에서 A지갑이 20 SToken을 판매 테스트 시작 *****");
+
+        CEX.Asset a = cex.getAssetMap().get("a");
+
+        cex.sale("a", "SToken", 20);
+
+        System.out.println("a의 현금 보유액은 20000원: " + (a.getMoney() == 20000));
+        System.out.println("a의 보유 SToken은 180개: " + (a.getSToken() == 180));
+        System.out.println("***** CEX에서 A지갑이 20 SToken을 판매 테스트 완료 *****");
     }
 }
